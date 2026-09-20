60af0e5056c300ea2c4ffbc080e15fcd42ae40bd
Michael Pratt <mpratt@protonmail.com>
2026-09-20 10:56:57 -0500

    Fix popularity update order

--- src/pages/api/items/[slug].ts
+++ src/pages/api/items/[slug].ts
@@ -4,13 +4,13 @@ import { env } from "cloudflare:workers";
 
 export async function GET({ params, request }: { params: Record<string, string>; request: Request; locals: any }) {
     const { ASTRO_TEST_KV } = env;
-    const value = await ASTRO_TEST_KV.get("popularity_" + params.slug);
+    let value = await ASTRO_TEST_KV.get("popularity_" + params.slug);
     if (value === null) {
-        await ASTRO_TEST_KV.put("popularity_" + params.slug, "1");
+        value = "1";
     } else {
-        const newValue = (parseInt(value) + 1).toString();
-        await ASTRO_TEST_KV.put("popularity_" + params.slug, newValue);
+        value = (parseInt(value) + 1).toString();        
     }
+    await ASTRO_TEST_KV.put("popularity_" + params.slug, value);
     return new Response(
         JSON.stringify({
             message: "Hello from the API test route!",
